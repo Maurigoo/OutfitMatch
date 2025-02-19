2acc08e848d1f7d987582524b778742a4b9a85dd
JoseMauricioHerbasBe <asd@gmail.es>
2025-02-19 19:33:06 +0100

    add gif Hanger

--- app/src/main/java/com/example/outfitmatch/Clothes.java
+++ app/src/main/java/com/example/outfitmatch/Clothes.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Clothes extends AppCompatActivity {
@@ -16,6 +18,12 @@ public class Clothes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothes);
+
+        // Cargar el GIF usando Glide
+        ImageView gifImageView = findViewById(R.id.gifImageView);  // El ImageView donde mostrarás el GIF
+        Glide.with(this)
+                .load(R.drawable.hanger_animation)  // Reemplaza con tu archivo GIF
+                .into(gifImageView);
 
         ImageButton botonShirts = findViewById(R.id.botonShirts);
         ImageButton botonPants = findViewById(R.id.botonPants);
