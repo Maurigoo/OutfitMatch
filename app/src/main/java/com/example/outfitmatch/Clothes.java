8c5e1f594748595391df0ea8b6e03f319e4ee89a
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2025-02-20 18:46:37 +0100

    Arreglado el menu en clothes

--- app/src/main/java/com/example/outfitmatch/Clothes.java
+++ app/src/main/java/com/example/outfitmatch/Clothes.java
@@ -1,10 +1,15 @@
 package com.example.outfitmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.outfitmatch.modelo.negocio.GestorPrenda;
 import com.example.outfitmatch.modelo.entidad.Prenda;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
@@ -53,6 +58,28 @@ public class Clothes extends AppCompatActivity {
                 Log.d("Clothes", "Shoes: " + shoes);
                 Log.d("Clothes", "Dresses: " + dresses);
                 Log.d("Clothes", "Accessories: " + accessories);
+            }
+        });
+
+        // Configurar BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.boton_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                } else if (itemId == R.id.nav_clothes) {
+                    startActivity(new Intent(getApplicationContext(), Clothes.class));
+                } else if (itemId == R.id.nav_add) {
+                    startActivity(new Intent(getApplicationContext(), AddClothesAlbum.class));
+                } else if (itemId == R.id.nav_profile) {
+                    startActivity(new Intent(getApplicationContext(), Perfil.class));
+                }
+
+                overridePendingTransition(0, 0);
+                return true;
             }
         });
     }
