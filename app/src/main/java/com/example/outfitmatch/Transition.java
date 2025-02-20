95568f53197ff6bff7ab040799dd4cd18748cd69
Avril Pino Perez <avril.pino.perez@bootcamp-upgrade.com>
2025-02-20 16:06:31 +0100

    menu idea

--- app/src/main/java/com/example/outfitmatch/Transition.java
+++ app/src/main/java/com/example/outfitmatch/Transition.java
@@ -2,14 +2,18 @@ package com.example.outfitmatch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.outfitmatch.Outfits;
 import com.example.outfitmatch.R;
 import com.example.outfitmatch.adaptador.AdaptadorTransition;
 import com.example.outfitmatch.modelo.entidad.Prenda;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
@@ -34,6 +38,27 @@ public class Transition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transition);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.boton_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                } else if (itemId == R.id.nav_clothes) {
+                    startActivity(new Intent(getApplicationContext(), Clothes.class));
+                } else if (itemId == R.id.nav_add) {
+                    startActivity(new Intent(getApplicationContext(), AddClothesAlbum.class));
+                } else if (itemId == R.id.nav_profile) {
+                    startActivity(new Intent(getApplicationContext(), Perfil.class));
+                }
+
+                overridePendingTransition(0, 0);
+                return true;
+            }
+        });
 
         like = findViewById(R.id.botonLike);
         x = findViewById(R.id.botonX);
