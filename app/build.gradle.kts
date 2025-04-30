4ec3a5bc459bd71ac2249745c440bee1781e32d8
Avril2504 <avrilpino@gmail.com>
2025-04-30 20:57:49 +0200

    fondo start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,5 +81,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
+//Lottie
+    implementation ("com.airbnb.android:lottie:6.1.0")
+
 
 }
